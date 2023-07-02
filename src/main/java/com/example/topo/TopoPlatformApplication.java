3479f7750de69432cdeba374c888790a3350c0f4
Iris-2333 <429328398@qq.com>
2023-07-02 12:18:14 +0800

    feat: init

---
+++ src/main/java/com/example/topo/TopoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.topo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopoPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopoPlatformApplication.class, args);
+    }
+
+}
